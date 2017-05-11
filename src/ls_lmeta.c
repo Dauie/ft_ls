ebfa541cedb4aa88903b7a0dd95d199275e22d1e
Robert LUTT <ausdauerr@gmail.com>
2017-05-11 16:22:13 -0700

    almost done, just fix -t and account for nanoseconds.

--- src/ls_lmeta.c
+++ src/ls_lmeta.c
@@ -6,7 +6,7 @@
 /*   By: rlutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/10 17:29:07 by rlutt             #+#    #+#             */
-/*   Updated: 2017/05/11 10:51:40 by rlutt            ###   ########.fr       */
+/*   Updated: 2017/05/11 16:19:52 by rlutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 void		ls_putpermtype(struct stat *st)
 {
-	if (st->st_mode == S_IFREG)
+	if (st->st_mode & S_IFREG)
 		ft_putchar('-');
-	else if (st->st_mode == S_IFDIR)
+	else if (st->st_mode & S_IFDIR)
 		ft_putchar('d');
-	else if (st->st_mode == S_IFLNK)
+	else if (st->st_mode & S_IFLNK)
 		ft_putchar('l');
-	else if (st->st_mode == S_IFBLK)
+	else if (st->st_mode & S_IFBLK)
 		ft_putchar('b');
-	else if (st->st_mode == S_IFCHR)
+	else if (st->st_mode & S_IFCHR)
 		ft_putchar('c');
-	else if (st->st_mode == S_IFSOCK)
+	else if (st->st_mode & S_IFSOCK)
 		ft_putchar('s');
-	else if (st->st_mode == S_IFIFO)
+	else if (st->st_mode & S_IFIFO)
 		ft_putchar('p');
 }
 
@@ -42,6 +42,7 @@ void		ls_putperm(struct stat *st)
 	ft_putchar((st->st_mode & S_IROTH) ? 'r' : '-');
 	ft_putchar((st->st_mode & S_IWOTH) ? 'w' : '-');
 	ft_putchar((st->st_mode & S_IXOTH) ? 'r' : '-');
+	ft_putchar(' ');
 }
 
 //get the size going, then a blksize static var may work.
@@ -50,14 +51,17 @@ void				ls_putmeta(t_node *node, t_lsnfo *info)
 	struct stat		st;
 	struct passwd	*pw;
 	struct group	*gp;
+	char			*tmp;
 	char			time[MXNAMLEN];
 	
 	lstat(ls_dirjoin(info->cdir, node->name), &st);
 	ft_bzero(time, MXNAMLEN);
-	ls_putpermtype(&st);
+	tmp = ctime(&st.st_mtime);
+	ft_strcpy(time, &tmp[4]);
+	ft_strdel(&tmp);
 	ls_putperm(&st);
 	pw = getpwuid(st.st_uid);
 	gp = getgrgid(st.st_gid);
-	ft_printf("%2lld % 5s % 8s %.24s  %s\n", st.st_nlink,
-			pw->pw_name, gp->gr_name, ctime(&st.st_mtime), node->name);
+	ft_printf("%3lld % 6s %9s % 6lld  %.12s  %s\n", st.st_nlink, pw->pw_name,
+			gp->gr_name, st.st_size , time, node->name);
 }
